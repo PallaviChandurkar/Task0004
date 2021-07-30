a566e27aa7940c20a4ca1d845ebd82de2d5e8d8b
PallaviChandurkar <87759060+PallaviChandurkar@users.noreply.github.com>
2021-07-30 20:29:35 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,7 @@
+var req = new XMLHttpRequest();
+req.open('GET','https://api.covid19api.com/summary',true);
+req.send();
+req.onload = function() {
+    var data = JSON.parse(this.response);
+    console.log(data.Global);
+}
